c78c80629d068824b2e01a9f6b069b517f1042fc
maurogestoso <maurogestoso@gmail.com>
2025-10-30 16:31:57 +0100

    feat(db): create db depending on NODE_ENV

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,4 +1,19 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 
-export const db = drizzle(process.env.DB_FILE_NAME!);
+export const db = createDb({ env: process.env.NODE_ENV! });
+
+function createDb({ env }: { env: string }) {
+  if (env === "production") {
+    return drizzle({
+      connection: {
+        url: process.env.TURSO_CONNECTION_URL!,
+        authToken: process.env.TURSO_AUTH_TOKEN!,
+      },
+    });
+  } else if (env === "test") {
+    return drizzle(process.env.DB_TEST_FILE_NAME!);
+  }
+
+  return drizzle(process.env.DB_FILE_NAME!);
+}
