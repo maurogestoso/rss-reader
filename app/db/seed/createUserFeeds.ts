9c2edd09afa6a31d5066e088e938d2247bf1d95c
maurogestoso <maurogestoso@gmail.com>
2025-10-30 23:44:47 +0100

    fix(db/items): decode title

--- app/db/seed/createUserFeeds.ts
+++ app/db/seed/createUserFeeds.ts
@@ -3,6 +3,10 @@ import { hideBin } from "yargs/helpers";
 import { db } from "..";
 import { feedsTable, newItemsTable, usersTable } from "../schema";
 import { eq } from "drizzle-orm";
+import Parser from "rss-parser";
+import * as entities from "entities";
+
+const rssParser = new Parser();
 
 const argv = await yargs(hideBin(process.argv))
   .option("userId", {
@@ -27,37 +31,32 @@ type NewFeed = typeof feedsTable.$inferInsert;
 type NewItem = typeof newItemsTable.$inferInsert;
 
 // TODO: pull data from actual feed
-const rawFeed: NewFeed = {
-  title: "kottke.org",
-  link: "https://kottke.org",
-  feedUrl: "https://feeds.kottke.org/main",
-  userId: user.id,
-};
-const [{ id: feedId }] = await db
-  .insert(feedsTable)
-  .values(rawFeed)
-  .returning();
-console.info("Inserted feed with id:", feedId);
+const feedUrls = ["https://feeds.kottke.org/main"];
+feedUrls.forEach(async (feedUrl) => {
+  const feed = await rssParser.parseURL(feedUrl);
 
-const items: NewItem[] = [
-  {
-    title:
-      "Kara Walker Creates Haunted Beast From Butchered Confederate Statue",
-    link: "https://kottke.org/25/10/kara-walker-creates-haunted-beast-from-butchered-confederate-statue",
-    pubDate: new Date("2025-10-19T14:12:43Z"),
-    feedId,
-    userId: user.id,
-  },
-  {
-    title: "Real Photos That Look Fake",
-    link: "https://kottke.org/25/10/real-photos-that-look-fake",
-    pubDate: new Date("2025-10-17T10:15:26Z"),
-    feedId,
-    userId: user.id,
-  },
-];
+  const [insFeed] = await db
+    .insert(feedsTable)
+    .values({
+      title: feed.title!,
+      feedUrl: feed.feedUrl!,
+      link: feed.link!,
+      userId: user.id,
+    })
+    .returning();
+  console.info("Inserted feed:", insFeed);
 
-items.forEach(async (item) => {
-  const [{ id }] = await db.insert(newItemsTable).values(item).returning();
-  console.info("Inserted item with id:", id);
+  feed.items.slice(0, 10).forEach(async (item) => {
+    const [insItem] = await db
+      .insert(newItemsTable)
+      .values({
+        title: entities.decodeXML(item.title!.trim()),
+        link: item.link!,
+        pubDate: new Date(item.pubDate!),
+        feedId: insFeed.id,
+        userId: user.id,
+      })
+      .returning();
+    console.info("Inserted item:", insItem);
+  });
 });
