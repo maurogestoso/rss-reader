2af515b4d481a3b94fc2ac6914cc951581a035f4
Mauro Gestoso <maurogestoso@gmail.com>
2025-10-30 15:43:00 +0100

    feat(Layout): add title and icon

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -2,9 +2,10 @@ import { Link, Outlet } from "react-router";
 import { getUser } from "~/db/user";
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/layout";
+import { Rss } from "lucide-react"
 
-export function meta({}: Route.MetaArgs) {
-  return [{ title: "Build something" }, { name: "description", content: "" }];
+export function meta({ }: Route.MetaArgs) {
+  return [{ title: "RSS Reader" }, { name: "description", content: "My home-cooked RSS Reader" }];
 }
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -25,9 +26,10 @@ export default function AppLayout({ loaderData }: Route.ComponentProps) {
     <>
       <div className="max-w-5xl mx-auto h-screen">
         <header className="p-2 flex justify-between items-center pr-2 border-2 border-black">
-          <h1 className="font-bold text-lg">
+          <h1 className="font-bold text-xl flex items-center gap-1">
+            <Rss className="stroke-orange-600"/>
             <Link to="/">
-              <span className="text-md">🤔</span> Let's build something
+              RSS Reader
             </Link>
           </h1>
           <div>
