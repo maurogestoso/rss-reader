b71a75486c2e932e09875818a07645d6f664371c
maurogestoso <maurogestoso@gmail.com>
2025-10-30 16:54:11 +0100

    fix(db): connect to production environment

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,19 +1,8 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
 
-export const db = createDb({ env: process.env.NODE_ENV! });
-
-function createDb({ env }: { env: string }) {
-  if (env === "production") {
-    return drizzle({
-      connection: {
-        url: process.env.TURSO_CONNECTION_URL!,
-        authToken: process.env.TURSO_AUTH_TOKEN!,
-      },
-    });
-  } else if (env === "test") {
-    return drizzle(process.env.DB_TEST_FILE_NAME!);
-  }
-
-  return drizzle(process.env.DB_FILE_NAME!);
-}
+export const db = drizzle({
+  connection: {
+    url: process.env.DB_CONNECTION_STRING!,
+    authToken: process.env.DB_AUTH_TOKEN!,
+  },
+});
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
-import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./app/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
-  },
+  dialect: process.env.NODE_ENV === "production" ? "turso" : "sqlite",
+  dbCredentials:
+    process.env.NODE_ENV === "production"
+      ? {
+          url: process.env.DB_CONNECTION_STRING!,
+          authToken: process.env.DB_AUTH_TOKEN!,
+        }
+      : {
+          url: process.env.DB_CONNECTION_STRING!,
+        },
 });
