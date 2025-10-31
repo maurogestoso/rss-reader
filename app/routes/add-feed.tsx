84eee6f2da08b1509228c5319cd98ec2c0ec7ca4
maurogestoso <maurogestoso@gmail.com>
2025-10-31 18:09:25 +0100

    refactor(AddFeeds): insert new feed and items

--- app/routes/add-feed.tsx
+++ app/routes/add-feed.tsx
@@ -1,41 +1,48 @@
-import {
-  destroySession,
-  getSession,
-  getUserFromSession,
-} from "~/sessions.server";
-import type { Route } from "./+types/add-feed";
 import { redirect } from "react-router";
-import { insertFeed } from "~/db/feed";
+import type { Route } from "./+types/add-feed";
+
+import { getSession } from "~/sessions.server";
+import { getUser } from "~/db/user";
+import { insertFeed } from "~/db/feeds";
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await getSession(request.headers.get("Cookie"));
+
+  const userId = session.get("userId");
+  if (!userId) {
+    return redirect("/login");
+  }
+
+  const user = await getUser(userId);
+  if (!user) return redirect("/login");
+}
 
 export async function action({ request }: Route.ActionArgs) {
-  const session = await getSession(request.headers.get("Cookie"));
-  const user = await getUserFromSession(session);
-  if (!user) {
-    return redirect("/login", {
-      headers: {
-        "Set-Cookie": await destroySession(session),
-      },
-    });
-  }
   const form = await request.formData();
-  const feedUrl = form.get("feedUrl")?.toString();
-  if (!feedUrl) throw new Error("feedUrl field is empty");
-
-  const feed = await insertFeed({ feedUrl, userId: user.id });
-  redirect("/");
+  const feedUrl = form.get("feedUrl")?.toString()!;
+  await insertFeed(feedUrl);
+  return redirect("/");
 }
+
 export default function AddFeed() {
   return (
     <>
       <h2 className="font-bold text-2xl">Add feed</h2>
       <form method="POST">
-        <fieldset>
-          <label htmlFor="feedUrl">
-            <p>Feed URL</p>
-          </label>
-          <input type="text" id="feedUrl" name="feedUrl" autoFocus required />
-        </fieldset>
-        <input type="submit" value={"Add"} />
+        <input
+          type="text"
+          id="feedUrl"
+          name="feedUrl"
+          autoFocus
+          required
+          placeholder="Feed URL..."
+        />
+        <button
+          type="submit"
+          className="ml-2 px-3 py-1 rounded-lg bg-green-600 text-white"
+        >
+          Add
+        </button>
       </form>
     </>
   );
