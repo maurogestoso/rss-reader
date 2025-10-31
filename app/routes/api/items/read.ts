8f56bc3e508f486f9c582578e4ad31c6855b4b97
maurogestoso <maurogestoso@gmail.com>
2025-10-31 23:29:45 +0100

    fix(api/items): protect routes

--- app/routes/api/items/read.ts
+++ app/routes/api/items/read.ts
@@ -1,8 +1,13 @@
 import { redirect } from "react-router";
 import type { Route } from "./+types/read";
+
 import { markItemAsRead } from "~/db/items";
+import { ensureUser } from "~/sessions.server";
 
 export async function action({ request }: Route.ActionArgs) {
+  const user = await ensureUser(request);
+  if (!user) return redirect("/login");
+
   const form = await request.formData();
   console.info("/api/items/read", form.get("itemId"));
   const id = parseInt(form.get("itemId")?.toString()!);
--- app/routes/api/items/starred.ts
+++ app/routes/api/items/starred.ts
@@ -1,8 +1,13 @@
 import { redirect } from "react-router";
 import type { Route } from "./+types/read";
+
 import { markItemAsStarred } from "~/db/items";
+import { ensureUser } from "~/sessions.server";
 
 export async function action({ request }: Route.ActionArgs) {
+  const user = await ensureUser(request);
+  if (!user) return redirect("/login");
+
   const form = await request.formData();
   console.info("/api/items/starred", form.get("itemId"));
   const id = parseInt(form.get("itemId")?.toString()!);
