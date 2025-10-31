f42894372bc49cbd39ebfb656ddb40f4ee4a7997
maurogestoso <maurogestoso@gmail.com>
2025-10-31 21:33:52 +0100

    refactor: name of the app

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -2,10 +2,13 @@ import { Link, Outlet } from "react-router";
 import { getUser } from "~/db/user";
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/layout";
-import { Rss } from "lucide-react"
+import { Coffee } from "lucide-react";
 
-export function meta({ }: Route.MetaArgs) {
-  return [{ title: "RSS Reader" }, { name: "description", content: "My home-cooked RSS Reader" }];
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Morning Coffee Reader" },
+    { name: "description", content: "My home-cooked RSS Reader" },
+  ];
 }
 
 export async function loader({ request }: Route.LoaderArgs) {
@@ -27,10 +30,8 @@ export default function AppLayout({ loaderData }: Route.ComponentProps) {
       <div className="max-w-5xl mx-auto h-screen">
         <header className="p-2 flex justify-between items-center pr-2 border-2 border-black">
           <h1 className="font-bold text-xl flex items-center gap-1">
-            <Rss className="stroke-orange-600"/>
-            <Link to="/">
-              RSS Reader
-            </Link>
+            <Coffee className="stroke-orange-600" />
+            <Link to="/">Morning Coffee Reader</Link>
           </h1>
           <div>
             {user ? (
