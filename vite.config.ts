31459ed51562d66465848af115b51c972ba184b0
maurogestoso <maurogestoso@gmail.com>
2025-11-02 14:48:23 +0100

    delete: internal auth-tokens logic (overkill)

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    allowedHosts: ["6342a74d2b51.ngrok-free.app"],
+  },
 });
