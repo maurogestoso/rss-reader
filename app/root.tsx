708d4286042b1b0a1f38a1aa0d1b89780d222989
maurogestoso <maurogestoso@gmail.com>
2025-11-03 02:07:16 +0100

    feat(root): add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,12 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [];
+export const links: Route.LinksFunction = () => [
+  {
+    rel: "icon",
+    href: "data:image/svg+xml,&lt;svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22&gt;&lt;text y=%22.9em%22 font-size=%2290%22&gt;☕&lt;/text&gt;&lt;/svg&gt;",
+  },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
